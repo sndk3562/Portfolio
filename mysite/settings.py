e7d4c93617936a156976ca924bb8db9a8f33d801
sndk3562 <nekonoinotora@gmail.com>
2022-04-14 17:07:52 +0900

    new

--- mysite/settings.py
+++ mysite/settings.py
@@ -73,10 +73,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
-#
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+#
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
