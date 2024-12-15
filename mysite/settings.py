aab775bbf090118a9561728db58670efee20afdd
sndk3562 <nekonoinotora@gmail.com>
2024-12-15 19:50:05 +0900

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = 'ca(x6rx6utl#bvv2oj1(i-x%stfr+rn7ehxp)9(!1pv6=z&x_%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://port-ko-dd6b03500230.herokuapp.com/']
-ALLOWED_HOSTS =  ['https://dashboard.heroku.com/apps/port-ko/deploy/github']
+ALLOWED_HOSTS = [os.environ.get("HOST")]
+
 
 CSRF_TRUSTED_ORIGINS    = [ "https://port-ko-dd6b03500230.herokuapp.com/" ]
 
